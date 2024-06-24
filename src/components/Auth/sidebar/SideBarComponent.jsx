f35cfd247a4e5bc195d0ab77b1816ec57ba3f12d
Rohith-AIDS <cchin2429@gmail.com>
2024-06-24 10:40:19 +0700

    scrollable sidebar

--- src/components/Auth/sidebar/SideBarComponent.jsx
+++ src/components/Auth/sidebar/SideBarComponent.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
-import { Menu } from 'semantic-ui-react'
-import "../sidebar/SideBarComponent.css"
-import UserInfoComponent from './UserInfo/UserInfoComponent'
-import ChannelComponent from './Channels/ChannelComponent'
-import PrivateChatComponent from './PrivateChat/PrivateChat.component'
-import ParentComponent from '../../ParentComponent/ParentComponent'
-
-
-
+import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import "../sidebar/SideBarComponent.css";
+import UserInfoComponent from './UserInfo/UserInfoComponent';
+import ChannelComponent from './Channels/ChannelComponent';
+import PrivateChatComponent from './PrivateChat/PrivateChat.component';
 
 const SideBarComponent = () => {
-  return (
-    <Menu vertical fixed='left' borderless size="large" className='side_bar'>
-        <UserInfoComponent/>
-        <ChannelComponent/>
-        <PrivateChatComponent/>
-    </Menu>
-  )
-}
+    return (
+        <Menu vertical fixed='left' borderless size="large" className='side_bar'>
+            <UserInfoComponent/>
+            <ChannelComponent/>
+            <PrivateChatComponent/>
+        </Menu>
+    );
+};
 
-export default SideBarComponent
+export default SideBarComponent;
